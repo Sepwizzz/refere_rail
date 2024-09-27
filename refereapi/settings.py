fae2231b2bc30f9891d5b0bca553b838b3682f09
Sepwizzz <115958805+Sepwizzz@users.noreply.github.com>
2024-09-27 14:38:18 -0500

    Update settings.py

--- refereapi/settings.py
+++ refereapi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k#+wcxd*n+4!n%_^#veg9*86zyajbjgn6cvuvt&ga^v1**f7_t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['localhost','refererail-production.up.railway.app']
 #lo mismo pero con la ip del pc ALLOWED_HOSTS = ["192.168.101.76"]
 # ALLOWED_HOSTS = ["192.168.101.76"]
 #mas esto al ejecutar python manage.py runserver 0.0.0.0:8000
